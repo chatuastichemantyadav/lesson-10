27aa4d53100055b3b74e822846df0dd375ff777b
chartastichemantyadav <hemantyadav0727@gmail.com>
2025-02-16 11:50:17 +0530

    publishing...

---
+++ after.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+result = base ** exponent  
+
+print(f"{base} raised to the power {exponent} is: {result}")
